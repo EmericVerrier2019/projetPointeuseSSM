2d1da28e476d845d8d50b87b7f9d0bce1132aba0
SchauQuentin <quentin.schau@etu.univ-tours.fr>
2021-05-12 10:19:56 +0200

    Ajout attribut classe CEmployee

--- src/projetPointeuseSSM/CEmployee.java
+++ src/projetPointeuseSSM/CEmployee.java
@@ -1,10 +1,20 @@
 package projetPointeuseSSM;
 
+/**
+ * The class used to model an employee
+ * 
+ * @author Quentin
+ * @version 1.0
+ * 
+ */
+
+
 public class CEmployee {
 
 	private int iEMPIdEmployee;
 	private String sEMPLastNameEmployee;
 	private String sEMPFirstNameEmployee;
 	private int iEMPNumeroDepartement;
+	private CPlanning plaEMPPlanningEmployee;
 	
 }

aa03a39050b18f7f1d85fa26b3b1774b4020ac4a
Matthis Servoz <matthis.servoz@etu.univ-tours.fr>
2021-05-12 10:02:57 +0200

    CDepartement (V1)

---
+++ src/projetPointeuseSSM/CDepartement.java
@@ -0,0 +1,12 @@
+package projetPointeuseSSM;
+import java.util.ArrayList;
+
+
+public class CDepartement {
+
+	private String sDepartementName;
+	private int iEmployeeNumber;
+	private ArrayList<String> asEmployee;
+	
+	
+}

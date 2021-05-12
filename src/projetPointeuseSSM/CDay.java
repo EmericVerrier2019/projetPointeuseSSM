ff8a733900dbaf423208a966d51dec363dc379f1
root <root@pcPatron>
2021-05-12 10:22:53 +0200

    maj planning et date

--- src/projetPointeuseSSM/CDay.java
+++ src/projetPointeuseSSM/CDay.java
@@ -8,5 +8,18 @@ public class CDay {
 	private LocalTime timeStart;
 	private LocalTime timeEnd;
 	
+	public CDay() 
+	{
+		timeStart = LocalTime.now();
+		timeEnd = LocalTime.now();
+		dayName = new String("");
+		
+	}
+	public CDay(LocalTime timeStart, LocalTime timeEnd, String dayName) 
+	{
+		this.timeStart = timeStart;
+		this.timeEnd = timeEnd;
+		this.dayName = dayName;
+	}
 	
 }
--- src/projetPointeuseSSM/CPlanning.java
+++ src/projetPointeuseSSM/CPlanning.java
@@ -1,6 +1,12 @@
 package projetPointeuseSSM;
 
-public class CPlanning {
-	
+import java.util.ArrayList;
 
+public class CPlanning {
+	private ArrayList<CDay> dayList;
+
+	public CPlanning() 
+	{
+		dayList = new ArrayList<CDay>();
+	}
 }

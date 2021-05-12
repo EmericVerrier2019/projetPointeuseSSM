7f66f16f425a44cb74a6900e6208383b40fde7ef
Matthis Servoz <matthis.servoz@etu.univ-tours.fr>
2021-05-12 10:20:31 +0200

    Departement (V1)

--- src/projetPointeuseSSM/CDepartement.java
+++ src/projetPointeuseSSM/CDepartement.java
@@ -4,9 +4,37 @@ import java.util.ArrayList;
 
 public class CDepartement {
 
-	private String sDepartementName;
-	private int iEmployeeNumber;
-	private ArrayList<String> asEmployee;
+	
+	//variables
+	
+	private String sDEPDepartementName;
+	private int iDEPDepartementNumber;
+	private int iDEPEmployeeNumber;
+	private ArrayList<CEmployee> asDEPEmployee;
+	
+	//setters - getters
+	
+	/**
+	 * Set the departement name
+	 * @author Matthis
+	 * @param sDEPDepartementName 
+	 * @return void 
+	 */
+	public void setDepartementName(String Name) {
+		sDEPDepartementName = Name;
+	}
+	
+	/**
+	 * Get the departement name
+	 * @author Matthis
+	 * @param /
+	 * @return sDEPDepartementName
+	 */
+	public String getDepartementName() {
+		return sDEPDepartementName;
+	}
+	
+	
 	
 	
 }

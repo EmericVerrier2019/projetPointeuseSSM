aad0d474cb33cb97cad4215c3d43a6edfac6270d
SchauQuentin <quentin.schau@etu.univ-tours.fr>
2021-05-12 10:04:10 +0200

    Classe CEmployee

---
+++ src/projetPointeuseSSM/CEmployee.java
@@ -0,0 +1,10 @@
+package projetPointeuseSSM;
+
+public class CEmployee {
+
+	private int iEMPIdEmployee;
+	private String sEMPLastNameEmployee;
+	private String sEMPFirstNameEmployee;
+	private int iEMPNumeroDepartement;
+	
+}

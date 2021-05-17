6f01b91d6ac854064b37995adef77782a5dfcd58
root <root@pcPatron>
2021-05-17 09:37:22 +0200

    changement javadoc @author

--- src/projetPointeuseSSM/Day.java
+++ src/projetPointeuseSSM/Day.java
@@ -7,7 +7,8 @@ public class Day {
 	/**
 	 * the three attributes of the CDayClass correspond to the moment in a day where an employee begin
 	 * to work, when he ends his day and finally, the name of the day in the week. The goal of this class is to represent a day of work in
-	 * an employee timetable
+	 * an employee timetable. 
+	 * @author Emeric Verrier
 	 */
 	private String sDAYdayName;
 	private LocalTime lDAYtimeStart;
@@ -17,7 +18,6 @@ public class Day {
 	/**
 	 * the default constructor of the CDay class, by default, the current time is used to define the beginning and the end of the employee day of work
 	 * and the name of the day is willingly empty.
-	 * @author Emeric Verrier
 	 * @see LocalTime
 	 */
 	public Day() 
@@ -28,7 +28,6 @@ public class Day {
 		
 	}
 	/**
-	 * @author Emeric Verrier
 	 * @param timeStart
 	 * @param timeEnd
 	 * @param dayName
@@ -41,7 +40,6 @@ public class Day {
 	}
 	/**
 	 * basic setter to set the moment when the employee starts to work
-	 * @author Emeric Verrier
 	 * @param timeStart
 	 */
 	public void setlDAYTimeStart(LocalTime timeStart) 
@@ -50,7 +48,6 @@ public class Day {
 	}
 	/**
 	 * basic setter to set the moment when the employee ends his day of work
-	 * @author Emeric Verrier
 	 * @param timeEnd
 	 */
 	public void setlDAYTimeEnd(LocalTime timeEnd) 
@@ -59,7 +56,6 @@ public class Day {
 	}
 	/**
 	 * getter method to get the moment when the employee ends his day of work
-	 * @author Emeric Verrier
 	 * @return lDAYTimeEnd
 	 */
 	public LocalTime getlDAYTimeEnd()
@@ -67,7 +63,6 @@ public class Day {
 		return  this.lDAYtimeEnd;
 	}
 	/**
-	 * @author Emeric Verrier
 	 * @return getter method to get the moment when the employee begins his work.
 	 */
 	public LocalTime getlDAYTimeStart() 

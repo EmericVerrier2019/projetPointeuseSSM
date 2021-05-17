9c730ba23d412b57af4b03b13f9c88a2f23e4c78
SchauQuentin <quentin.schau@etu.univ-tours.fr>
2021-05-17 16:48:42 +0200

    Test unitaire fonctionnelle pour employee

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -1,28 +1,28 @@
 package test;
 
-import projetPointeuseSSM.Employee;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
-
-import java.time.LocalTime;
-import java.util.Arrays;
-import java.util.Collection;
-
+import java.time.*;
+import java.util.*;
 
 import org.junit.*;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.*;
 
 
-public final class EmployeeTest {
+import projetPointeuseSSM.Employee;
+
+@RunWith(value = Parameterized.class)
+public class EmployeeTest {
 
 	@BeforeClass
 	public static void ShowBeforeRunTestClass(){
-	    System.out.println("Start tests in CalculatorTest");
+	    System.out.println("Start tests in EmployeeTest");
 	}
 	@AfterClass
 	public static void ShowAfterAllTestsRunnedInClass(){
-	    System.out.println("All tests have been runned in CalculatorTest");
+	    System.out.println("All tests have been runned in EmployeeTest");
 	}
 	@Before
 	public void ShowBeforeRunEachTest(){
@@ -40,30 +40,31 @@ public final class EmployeeTest {
 	@Parameter(value = 1)
 	public LocalTime expected;
 	
-	@Parameters(name = "{index}: testAddHourDo({0}) = {1}")
+	@Parameter(value = 2)
+	public LocalTime initialHourDo;
+	@Parameters(name = "{index}: testAddHourDo(getHoursDo(): {2};addHoursDo({0});"+
+						"  => employee.getHoursDo: {1}")
 	public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {LocalTime.of(0,0,20),LocalTime.of(1,10,30)},
-                {LocalTime.of(0,20,0),LocalTime.of(1,30,10)},
-                {LocalTime.of(2,0,0),LocalTime.of(3,10,10)},
-                {LocalTime.of(0,20,20),LocalTime.of(1,30,30)},
-                {LocalTime.of(2,0,20),LocalTime.of(3,10,30)},
-                {LocalTime.of(2,20,0),LocalTime.of(3,30,10)},
-                {LocalTime.of(2,20,20),LocalTime.of(3,30,30)}
+                {LocalTime.of(0,0,20),LocalTime.of(1,10,30),LocalTime.of(1, 10, 10)},
+                {LocalTime.of(0,20,0),LocalTime.of(1,30,10),LocalTime.of(1, 10, 10)},
+                {LocalTime.of(2,0,0),LocalTime.of(3,10,10),LocalTime.of(1, 10, 10)},
+                {LocalTime.of(0,20,20),LocalTime.of(1,30,30),LocalTime.of(1, 10, 10)},
+                {LocalTime.of(2,0,20),LocalTime.of(3,10,30),LocalTime.of(1, 10, 10)},
+                {LocalTime.of(2,20,0),LocalTime.of(3,30,10),LocalTime.of(1, 10, 10)},
+                {LocalTime.of(2,20,20),LocalTime.of(3,30,30),LocalTime.of(1, 10, 10)}
         });
     }
 	
 	@Test
     public void testAddHourDo() {
-        Employee test = new Employee();
+        Employee employee = new Employee();
         //Set 1h 10m 10s, the time that employee has done 
-        test.setHoursDo(LocalTime.of(1, 10, 10));
-        test.addHourDo(hourAdded);
-        assertEquals(expected, test.getHoursDo());
+        employee.setHoursDo(initialHourDo);
+        employee.addHourDo(hourAdded);
+        assertEquals(expected, employee.getHoursDo());
 		
     }
 	
-	
-	
-	
+
 }

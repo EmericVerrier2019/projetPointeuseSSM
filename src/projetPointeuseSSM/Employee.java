49dee48f0b9627deabaaa830a6d0449b4b5bfefc
SchauQuentin <quentin.schau@etu.univ-tours.fr>
2021-05-17 09:31:50 +0200

    mise au propre

--- src/projetPointeuseSSM/Employee.java
+++ src/projetPointeuseSSM/Employee.java
@@ -31,12 +31,45 @@ public class Employee {
 	private Planning planningEmployee;
 	private LocalTime hoursTheory;
 	private LocalTime hoursDo;
+	private boolean isWorking; //this boolean mean the employee is present at work
 	
 	/***********************/
 	/***** Constructor *****/
 	/***********************/
 	
+	/**
+	 * Default constructor, which create Employee
+	 */
+	public Employee(){
+		setHoursDo(LocalTime.of(0, 0, 0));
+		setHoursTheory(LocalTime.of(0, 0, 0));
+	}
 	
+	/**
+	 * Constructor which create a employee with the first name, the last name and the Employee's id of department
+	 * @param String firstName: the first name, String lastName: the last name,int idDepartment: the id of department,
+	 * 			int idEmployee : the Employee's id
+	 */
+	public Employee(String firstName,String lastName, int idDepartment, int idEmployee) {
+		setFirstNameEmployee(firstName);
+		setLastNameEmployee(lastName);
+		setIdDepartment(idDepartment);
+		setIdEmployee(idEmployee);
+		setHoursDo(LocalTime.of(0, 0, 0));
+		setHoursTheory(LocalTime.of(0, 0, 0));
+	}
+	
+	/**
+	 * Constructor which create a employee with the id of the employee and the if of the department
+	 * @param int idDepartment: the id of department,int idEmployee : the Employee's id
+	 * 
+	 */
+	public Employee(int idDepartment, int idEmployee) {
+		setIdDepartment(idDepartment);
+		setIdEmployee(idEmployee);
+		setHoursDo(LocalTime.of(0, 0, 0));
+		setHoursTheory(LocalTime.of(0, 0, 0));
+	}
 	
 	/******************/
 	/***** Setter *****/
@@ -101,6 +134,14 @@ public class Employee {
 		this.hoursDo = newHour;
 	}
 	
+	/**
+	 * Update the attribute isWorking
+	 * @param boolean isWorking, we want to set if the employee is present at work
+	 */
+	public void setIsWorking(boolean isWorking) {
+		this.isWorking = isWorking;
+	}
+	
 	/******************/
 	/***** Getter *****/
 	/******************/
@@ -159,5 +200,13 @@ public class Employee {
 	 */
 	public LocalTime getHoursDo() {
 		return this.hoursDo;
+	}
+	
+	/**
+	 * Return the attribute isWorking
+	 * @return boolean isWorking
+	 */
+	public boolean getIsWorking() {
+		return this.isWorking;
 	}
 }

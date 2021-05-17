1dff96308a48eb29cd41b8eaadc4cd1a2e85d8f7
Theo Millaire <theo.millaire@etu.univ-tours.fr>
2021-05-17 10:13:29 +0200

    Get et Set timeclock

--- src/projetPointeuseSSM/TimeClock.java
+++ src/projetPointeuseSSM/TimeClock.java
@@ -1,7 +1,45 @@
 package projetPointeuseSSM;
-i
+import java.util.ArrayList;
 
 public class TimeClock {
-	private Planning 
-
+	//variable 
+	private ArrayList<Employee> asDEPEmployeeWork;
+	private ArrayList<Employee> asDEPEmployeeHome;
+	
+	/*****************
+	 * Constructors
+	 ****************/
+	
+	/*****************
+	 * Methods
+	 ****************/
+	
+	/*
+	 * @brief Get the list of workers 
+	 * @return ArrayList<Employee>
+	 */
+	public ArrayList<Employee> GetListofEmployeeWork(){
+		return asDEPEmployeeWork;
+	}
+	/*
+	 * @brief Get the list of workers at home
+	 * @return ArrayList<Employee>
+	 */
+	public ArrayList<Employee> GetListofEmployeeHome(){
+		return asDEPEmployeeHome;
+	}
+	/*
+	 * @brief Set the list of workers 
+	 * @param ArgEmployeeWork : A list of workers
+	 */
+	public void SetListofEmployeeWork(ArrayList<Employee> ArgEmployeeWork){
+		this.asDEPEmployeeWork = ArgEmployeeWork;
+	}
+	/*
+	 * @brief Set the list of workers at home
+	 * @param ArgEmployeeHome : A list of workers at home
+	 */
+	public void SetListofEmployeeHome(ArrayList<Employee> ArgEmployeeHome){
+		this.asDEPEmployeeHome = ArgEmployeeHome;
+	}
 }

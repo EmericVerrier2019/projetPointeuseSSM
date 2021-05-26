2a6c890c2f4b14c27f2a79d438067231c590abfc
Theo Millaire <theo.millaire@etu.univ-tours.fr>
2021-05-26 09:12:12 +0200

    Creation de l'entreprise dans le main pour les tests a faire avec la pointeuse

--- src/projetPointeuseSSM/Main.java
+++ src/projetPointeuseSSM/Main.java
@@ -7,6 +7,7 @@ import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.List;
 
 import javax.swing.SwingUtilities;
 import view.*;
@@ -111,12 +112,14 @@ public class Main {
 		//tutoriel pour montrer un peu l'utilisation : https://www.vogella.com/tutorials/JavaDateTimeAPI/article.html
 	}
 	
-	/*public void Strump() {
+	public void Strump() {
 		//Creation of four employee
 		Employee Employe1 = new Employee();
 		Employee Employe2 = new Employee();
 		Employee Employe3 = new Employee();
 		Employee Employe4 = new Employee();
+		Employee Employe5 = new Employee();
+		Employee Employe6 = new Employee();
 		//Make the employee list for department
 		ArrayList<Employee> EmployeeListe = new ArrayList<Employee>();
 		EmployeeListe.add(Employe1);
@@ -124,10 +127,14 @@ public class Main {
 		EmployeeListe.add(Employe3);
 		EmployeeListe.add(Employe4);
 		
+		ArrayList<Employee> EmployeeListeHome = new ArrayList<Employee>();
+		EmployeeListeHome.add(Employe5);
+		EmployeeListeHome.add(Employe6);
+		
 		Department Departement = new Department("DI",7,4,EmployeeListe);
 		
 		//Creation of the planning
-		LocalTime start = LocalTime.of(8, 0);
+		/*LocalTime start = LocalTime.of(8, 0);
 		LocalTime end = LocalTime.of(16, 0);
 		
 		Day Lundi = new Day(start,end,"Lundi");
@@ -136,6 +143,23 @@ public class Main {
 		Day Jeudi = new Day(start,end,"Jeudi");
 		Day Vendredi = new Day(start,end,"Vendredi");
 		
+		List<Day> tousLesJours = new ArrayList<>();
+		tousLesJours.add(Lundi);
+		tousLesJours.add(Mardi);
+		tousLesJours.add(Mercredi);
+		tousLesJours.add(Jeudi);
+		tousLesJours.add(Vendredi);*/
 		
-	}*/
+		Planning planning = new Planning();
+		planning.PlanningStub();
+		
+		//Creation time clock
+		TimeClock pointeuse = new TimeClock(EmployeeListe,EmployeeListeHome);
+		for(Employee employe : pointeuse.GetListofEmployeeWork()) {
+			employe.setPlanning(planning);
+			employe.toString();
+		}
+		
+		
+	}
 }

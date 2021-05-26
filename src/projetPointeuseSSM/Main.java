0e686bcd2538908fc1fce64734786d1e498cea24
Theo Millaire <theo.millaire@etu.univ-tours.fr>
2021-05-26 08:32:20 +0200

    rt

--- src/projetPointeuseSSM/Main.java
+++ src/projetPointeuseSSM/Main.java
@@ -5,6 +5,7 @@ import java.time.chrono.ChronoLocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import javax.swing.SwingUtilities;
@@ -109,4 +110,32 @@ public class Main {
 		
 		//tutoriel pour montrer un peu l'utilisation : https://www.vogella.com/tutorials/JavaDateTimeAPI/article.html
 	}
+	
+	/*public void Strump() {
+		//Creation of four employee
+		Employee Employe1 = new Employee();
+		Employee Employe2 = new Employee();
+		Employee Employe3 = new Employee();
+		Employee Employe4 = new Employee();
+		//Make the employee list for department
+		ArrayList<Employee> EmployeeListe = new ArrayList<Employee>();
+		EmployeeListe.add(Employe1);
+		EmployeeListe.add(Employe2);
+		EmployeeListe.add(Employe3);
+		EmployeeListe.add(Employe4);
+		
+		Department Departement = new Department("DI",7,4,EmployeeListe);
+		
+		//Creation of the planning
+		LocalTime start = LocalTime.of(8, 0);
+		LocalTime end = LocalTime.of(16, 0);
+		
+		Day Lundi = new Day(start,end,"Lundi");
+		Day Mardi = new Day(start,end,"Mardi");
+		Day Mercredi = new Day(start,end,"Mercredi");
+		Day Jeudi = new Day(start,end,"Jeudi");
+		Day Vendredi = new Day(start,end,"Vendredi");
+		
+		
+	}*/
 }
